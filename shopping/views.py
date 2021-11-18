c2ba60049fb27bdd4021ca451e19577e18380b95
henrique77 <henriquecaires@outlook.com>
2021-11-18 16:19:09 -0300

    Primeiro commit

---
+++ shopping/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from .models import ShoppingItem
+from .serializers import ShoppingItemSerializer
+
+
+class ShoppingItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ShoppingItemSerializer
+    queryset = ShoppingItem.objects.all()
